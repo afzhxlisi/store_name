24b9df20e9791a41a952bf114a86a11fc4f29d2d
wang <afzhxliisi@126.com>
2017-04-21 18:43:24 +0800

    add lianjia

--- spiders/domz.py
+++ spiders/domz.py
@@ -12,48 +12,59 @@ class CrawlSpider(scrapy.Spider):
         items = []
         item=StoreNameItem()
         #length = len(response.xpath("//*[@id='house-lst']/li[1]/div[2]/h2/a/@href"))
-        length = len(response.xpath("//*[contains(@class,'row1-text')]/text()"))
+        length = len(response.xpath("//*[contains(@class,'total-price')]/text()"))
         print length
-
-        name=response.xpath("//*[contains(@class,'row1-text')]/text()")[1].extract()
-        comname=response.xpath("//*[contains(@class,'total-price')]/text()")[0].extract()
-        type=response.xpath("//*[contains(@class,'row2-text')]/a[@class='laisuzhou']/@href")[0].extract()
-        area=response.xpath("//*[@class='prop-title']/a/@href")[0].extract()
-
-        '''name=response.xpath("//*[@id='house-lst']/li[1]/div[2]/h2/a/@href")[0].extract()
-        comname=response.xpath('//*[@id="house-lst"]//*[@class="nameEllipsis"]/text()')[0].extract()
-        type=response.xpath('//*[@id="house-lst"]//*[@class="nameEllipsis"]/../following-sibling::*[1]/text()')[0].extract()
-        area=response.xpath('//*[@id="house-lst"]//*[@class="nameEllipsis"]/../following-sibling::*[2]/text()')[0].extract()
-        price=response.xpath('//*[@id="house-lst"]//*[@class="price"]/span/text()')[0].extract()
-        num=response.xpath('//*[@id="house-lst"]//*[@class="square"]//*[@class="num"]/text()')[0].extract()
-        fangurl=response.xpath('//*[@id="house-lst"]//div[@class="info-panel"]//*[@name="selectDetail"]/@href')[0].extract()
-        comurl=response.xpath('//*[@id="house-lst"]//*[@class="nameEllipsis"]/../@href')[0].extract()
-        response.css("");
-        item["name"]=name
-        item["comname"]=comname
-        item["type"]=type
-        item["area"]=area
-        item["price"]=price
-        item["num"]=num
-        item["fangurl"]=fangurl
-        item["comurl"]=comurl'''
-        item["name"]=name
-        item["comname"]=comname
-        item["type"]=type
-        item["area"]=area
-        #//*[contains(@class,'row1-text')]/text()
-        #//*[contains(@class,'total-price')]/text()
-        #//*[contains(@class,'row2-text')]/a[@class='laisuzhou']/@href
-        #//*[@class='prop-title']/a/@href
-        #
-        #//*[@id="js-ershoufangList"]/div[2]/div[3]/div/ul/li[1]/div/div[1]/a
-        #0 type
-        #1 area
-        #'//*[@id="house-lst"]//*[@class="price"]/span'
-        #//*[@id="house-lst"]//*[@class="square"]//*[@class="num"]/text()
-        #'//*[@id="house-lst"]//div[@class="info-panel"]//*[@name="selectDetail"]/@href') fangurl
-        #//*[@id="house-lst"]//*[@class="nameEllipsis"]/../@href comurl
-        yield item
+        for i in range(0,length-1):
+            name=response.xpath("//*[contains(@class,'row1-text')]/text()")[2*i+1].extract()
+            price=response.xpath("//*[contains(@class,'total-price')]/text()")[i].extract()
+            comurl=response.xpath("//*[contains(@class,'row2-text')]/a[@class='laisuzhou']/@href")[i].extract()
+            fangurl=response.xpath("//*[@class='prop-title']/a/@href")[i].extract()
+            comname=response.xpath("//*[contains(@class,'row2-text')]/a[@class='laisuzhou']/span/text()")[i].extract()
+            name=name.replace("\t","").replace("\n","").split("|")
+            type=name[0]
+            area=name[1]
+            '''name=response.xpath("//*[@id='house-lst']/li[1]/div[2]/h2/a/@href")[0].extract()
+            comname=response.xpath('//*[@id="house-lst"]//*[@class="nameEllipsis"]/text()')[0].extract()
+            type=response.xpath('//*[@id="house-lst"]//*[@class="nameEllipsis"]/../following-sibling::*[1]/text()')[0].extract()
+            area=response.xpath('//*[@id="house-lst"]//*[@class="nameEllipsis"]/../following-sibling::*[2]/text()')[0].extract()
+            price=response.xpath('//*[@id="house-lst"]//*[@class="price"]/span/text()')[0].extract()
+            num=response.xpath('//*[@id="house-lst"]//*[@class="square"]//*[@class="num"]/text()')[0].extract()
+            fangurl=response.xpath('//*[@id="house-lst"]//div[@class="info-panel"]//*[@name="selectDetail"]/@href')[0].extract()
+            comurl=response.xpath('//*[@id="house-lst"]//*[@class="nameEllipsis"]/../@href')[0].extract()
+            response.css("");
+            item["name"]=name
+            item["comname"]=comname
+            item["type"]=type
+            item["area"]=area
+            item["price"]=price
+            item["num"]=num
+            item["fangurl"]=fangurl
+            item["comurl"]=comurl'''
+            item["name"]=name
+            item["area"]=area
+            item["type"]=type
+            item["price"]=price
+            item["fangurl"]=fangurl
+            item["comurl"]=comurl
+            item["comname"]=comname
+            items.append(item)
+            #items.append(item)
+            #//*[contains(@class,'row1-text')]/text()
+            #//*[contains(@class,'total-price')]/text()
+            #//*[contains(@class,'row2-text')]/a[@class='laisuzhou']/@href
+            #//*[@class='prop-title']/a/@href
+            #name.replace("\t","")
+            #name.split("|")
+            #
+            #//*[@id="js-ershoufangList"]/div[2]/div[3]/div/ul/li[1]/div/div[1]/a
+            #0 type
+            #1 area
+            #'//*[@id="house-lst"]//*[@class="price"]/span'
+            #//*[@id="house-lst"]//*[@class="square"]//*[@class="num"]/text()
+            #'//*[@id="house-lst"]//div[@class="info-panel"]//*[@name="selectDetail"]/@href') fangurl
+            #//*[@id="house-lst"]//*[@class="nameEllipsis"]/../@href comurl
+        for item in items:
+            yield item
         print(item["name"])
         self.page=self.page+1
         url ="http://sh.lianjia.com/ershoufang/d"+str(self.page)
